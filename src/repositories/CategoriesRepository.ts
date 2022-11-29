8b98ec7cbf98b9bbc4edfa2706e01652b12d722e
mano gui <guilhermedavidrk@gmail.com>
2022-11-29 14:15:21 -0300

    feat: add methods create, list

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,34 @@
+import { Category } from "../model/Category";
+
+// DTO => data transfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO) {
+    const category = new Category();
+
+    Object.assign(category, {
+      // atribui os parametros do body para a instancia category
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+}
+
+export { CategoryRepository };
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import { CategoryRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoryRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
+export { categoriesRoutes };
