09ca36336ca306de1de6100d61a1e8d9b9ce5842
mano gui <guilhermedavidrk@gmail.com>
2022-11-30 17:43:36 -0300

    feat: working with upload

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,18 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
