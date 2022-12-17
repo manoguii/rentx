02bb9e149f6d820e7772fa57aa384301c634bb40
manoguii <guilhermedavidrk@gmail.com>
2022-12-16 23:55:39 -0300

    :test_tube:test: create initial test using jest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Cria categoria", () => {
+  it("Espero que dois mais dois seja 4", () => {
+    const soma = 2 + 2;
+    const resultado = 5;
+
+    expect(soma).toBe(resultado);
+  });
+
+  it("Espero que dois mais dois nao seja 5", () => {
+    const soma = 2 + 2;
+    const resultado = 5;
+
+    expect(soma).not.toBe(resultado);
+  });
+});
