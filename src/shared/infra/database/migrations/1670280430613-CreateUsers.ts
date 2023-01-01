d39ff4fb4b23f1692483a0bd655885bea57cc6f3
manoguii <guilhermedavidrk@gmail.com>
2023-01-01 20:58:33 -0300

    feat: create table rentals in database

--- src/shared/infra/database/migrations/1670280430613-CreateUsers.ts
+++ src/shared/infra/database/migrations/1670280430613-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1670280430613 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
