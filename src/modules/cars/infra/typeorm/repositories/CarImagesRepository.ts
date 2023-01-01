8ad53c49de42b917e3ddb91f3fec3f9d9c072aae
manoguii <guilhermedavidrk@gmail.com>
2023-01-01 15:48:47 -0300

    :zap::zap::zap:feat: create uploadCarImage

---
+++ src/modules/cars/infra/typeorm/repositories/CarImagesRepository.ts
@@ -0,0 +1,26 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+
+import { CarImage } from "../entities/CarImage";
+
+class CarImagesRepository implements ICarImagesRepository {
+  private repository: Repository<CarImage>;
+
+  constructor() {
+    this.repository = getRepository(CarImage);
+  }
+
+  async create(car_id: string, image_name: string): Promise<CarImage> {
+    const carImage = this.repository.create({
+      car_id,
+      image_name,
+    });
+
+    await this.repository.save(carImage);
+
+    return carImage;
+  }
+}
+
+export { CarImagesRepository };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarImagesRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
@@ -22,3 +24,8 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarImagesRepository>(
+  "CarImagesRepository",
+  CarImagesRepository
+);
