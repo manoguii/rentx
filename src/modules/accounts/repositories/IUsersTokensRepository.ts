f7a8b746779a752357c01d808e1ebe712b7b54eb
manoguii <guilhermedavidrk@gmail.com>
2023-01-06 23:03:32 -0300

    :lock::lock: feat: Implement resetPasswordUser

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -9,6 +9,7 @@ interface IUsersTokensRepository {
   }: ICreateUserTokenDTO): Promise<UserToken>;
   findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserToken>;
   deleteById(id: string): Promise<void>;
+  findByRefreshToken(refresh_token: string): Promise<UserToken>;
 }
 
 export { IUsersTokensRepository };
--- src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
@@ -37,6 +37,10 @@ class DayJsDateProvider implements IDateProvider {
   addHours(hours: number): Date {
     return dayjs().add(hours, "hours").toDate();
   }
+
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
+  }
 }
 
 export { DayJsDateProvider };
